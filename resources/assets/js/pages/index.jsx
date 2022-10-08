1e3161e45ccfb217f3d79eaaf7b35589080b1ee8
Joao Julio <joaopjt@gmail.com>
2022-10-08 14:04:07 -0300

    Added generic insturments list

--- resources/assets/js/pages/index.jsx
+++ resources/assets/js/pages/index.jsx
@@ -30,6 +30,16 @@ export default class App extends Component {
               <Route path="/viola" element={<GenericInstrumentPage instrument="Viola"/>} />
               <Route path="/cello" element={<GenericInstrumentPage instrument="Cello"/>} />
               <Route path="/basson" element={<GenericInstrumentPage instrument="Bassoon"/>} />
+              <Route path="/harp" element={<GenericInstrumentPage instrument="Harp"/>} />
+              <Route path="/clavinet" element={<GenericInstrumentPage instrument="Clavinet"/>} />
+              <Route path="/vibraphone" element={<GenericInstrumentPage instrument="Vibraphone"/>} />
+              <Route path="/marimba" element={<GenericInstrumentPage instrument="Marimba"/>} />
+              <Route path="/xylophone" element={<GenericInstrumentPage instrument="Xylophone"/>} />
+              <Route path="/tubular-bells" element={<GenericInstrumentPage instrument="Tubular Bells"/>} />
+              <Route path="/eletric-guitar" element={<GenericInstrumentPage instrument="Eletric Guitar"/>} />
+              <Route path="/trumpet" element={<GenericInstrumentPage instrument="Trumped"/>} />
+              <Route path="/trombone" element={<GenericInstrumentPage instrument="Trombone"/>} />
+              <Route path="/tuba" element={<GenericInstrumentPage instrument="Tuba"/>} />
               <Route path="/app/:id" element={<ComposerPage />} />
               <Route path="/app" element={<InstrumentsPage />} />
               <Route path="/" element={<HomePage />} />
